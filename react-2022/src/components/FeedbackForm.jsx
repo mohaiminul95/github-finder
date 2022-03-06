69d62fd485af95089b5952b6583b483b04bc7b23
mohaiminul95 <rabidislam94@gmail.com>
2022-03-06 22:12:55 +0600

    form validation added

--- react-2022/src/components/FeedbackForm.jsx
+++ react-2022/src/components/FeedbackForm.jsx
@@ -5,8 +5,20 @@ import Button from './shared/Button';
 function FeedbackForm() {
 
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   function textHandler(e) {
+    if(text === null) {
+        setBtnDisabled(true)
+        setMessage('')
+    } else if(text !== null && text.trim().length <= 10) {
+        setBtnDisabled(true)
+        setMessage('Text must be at least 10 characters.')
+    } else {
+        setBtnDisabled(false)
+        setMessage(null)
+    }
     setText(e.target.value)
   }
     
@@ -16,8 +28,9 @@ function FeedbackForm() {
             <h2>How would you rate your service with us?</h2>
             <div className='input-group'>
                 <input onChange={textHandler} value={text} type='text' placeholder='Write a review' />
-                <Button type='submit'>Send</Button>
+                <Button type='submit' isDisabled={btnDisabled}>Send</Button>
             </div>
+            { message && <div className='message'>{message}</div> }
         </form>
     </Card>
   )
