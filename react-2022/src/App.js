b9a1dcdddd77adc4affcc18d2df01150d814429b
mohaiminul95 <rabidislam94@gmail.com>
2022-03-10 15:54:57 +0600

    react router added

--- react-2022/src/App.js
+++ react-2022/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid'
 import './App.css';
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import Card from './components/shared/Card';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import About from './pages/About';
+import AboutIcon from './components/AboutIcon';
 
 function  App() {
 
@@ -26,12 +30,31 @@ function  App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm addFeedback={addFeedback}/>
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList 
-          feedback={feedback} 
-          deleteItemHandler={deleteItemHandler}
-        />
+        <BrowserRouter>
+        <Card>
+          <NavLink to='/' className={({ isActive }) => isActive? "active": ''}>
+            Home
+          </NavLink>
+          <NavLink to='/about' className={({ isActive }) => isActive? "active": ''}>
+            About
+          </NavLink>
+        </Card>
+          <Routes>
+            <Route path='/' element={
+              <>
+                <FeedbackForm addFeedback={addFeedback}/>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList 
+                  feedback={feedback} 
+                  deleteItemHandler={deleteItemHandler}
+                />
+              </>
+            }>
+            </Route>
+            <Route path='about' element={<About />}/>
+          </Routes>
+          <AboutIcon />
+        </BrowserRouter>
       </div>
     </>
   )
