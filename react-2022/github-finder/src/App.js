82284b8438df40b4d1fe184d2815e5fecbf2b336
mohaiminul95 <rabidislam94@gmail.com>
2022-03-13 16:58:45 +0600

    github finder project initialized

---
+++ react-2022/github-finder/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p class="italic ...">The quick brown fox ...</p>
+      <p class="not-italic ...">The quick brown fox ...</p>
+    </>
+  );
+}
+
+export default App;
