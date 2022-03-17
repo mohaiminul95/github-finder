79e61603a364a9a5965711ef45b8ea382844090e
mohaiminul95 <rabidislam94@gmail.com>
2022-03-17 12:11:18 +0600

    navbar and about components are added

--- react-2022/github-finder/src/App.js
+++ react-2022/github-finder/src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <>
-      <p class="italic ...">The quick brown fox ...</p>
-      <p class="not-italic ...">The quick brown fox ...</p>
-    </>
+    <BrowserRouter>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          content
+        </main>
+      </div>
+      <Footer />        
+    </BrowserRouter>
   );
 }
 
