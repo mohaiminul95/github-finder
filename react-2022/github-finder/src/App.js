150305cde6791e93e32975b47d7be5ce6e0c1ce0
mohaiminul95 <rabidislam94@gmail.com>
2022-03-18 11:40:18 +0600

    basic UI is done

--- react-2022/github-finder/src/App.js
+++ react-2022/github-finder/src/App.js
@@ -1,7 +1,12 @@
-import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -9,7 +14,12 @@ function App() {
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
         <main className='container mx-auto px-3 pb-12'>
-          content
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/' element={<NotFound />} />
+          </Routes>
         </main>
       </div>
       <Footer />        
