de29bed5fce6c5ce3f831652228de021c7452be6
mohaiminul95 <rabidislam94@gmail.com>
2022-03-30 19:07:31 +0600

    menu created

--- test/src/App.js
+++ test/src/App.js
@@ -1,53 +1,38 @@
-import React, { useState, useReducer } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import "./App.css"
+import Home from './components/Home'
+import Feed from './components/Feed'
+import Calculator from './components/Calculator'
 
-const reducer = (state, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return {
-        ...state,
-        counter: state.counter + 1
-      }
-    case "DECREMENT":
-      return {
-        ...state,
-        counter: state.counter - 1
-      }
-    case "INIT":
-      return {
-        ...state,
-        counter: action.payload
-      }  
-    default:
-      return state
-  }
-}
 
-const initialState = {
-  counter: 0
-}
 
 function App() {
-  const [input, setInput] = useState(0)
-  const [state, dispatch] = useReducer(reducer, initialState)
-
   return (
-    <div>
-      <h1>Reducer Example</h1>
+    <BrowserRouter>
+      <div className="App">
+        <ul className="navStyle">
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/feed'>Feed</Link>
+          </li>
+          <li>
+            <Link to='/calculator'>Calculator</Link>
+          </li>
+        </ul>
+        <br />
+        <hr />
 
-      <input type='number'
-             onChange={(e) => setInput(parseInt(e.target.value))}
-             value={input}
-      />
-      <br/>
-      <button onClick={() => dispatch({type: 'INIT', payload: input})}>Initialize Counter</button>
-
-      <p>{ state.counter }</p>
-      <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
-      <button onClick={() => dispatch({type: 'DECREMENT'})}>Decrement</button>
-
-    </div>
-  );
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/feed' element={<Feed />} />
+            <Route path='/calculator' element={<Calculator />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ test/src/components/Calculator.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useReducer } from "react";
+
+const reducer = (state, action) => {
+    switch(action.type) {
+      case "INCREMENT":
+        return {
+          ...state,
+          counter: state.counter + 1
+        }
+      case "DECREMENT":
+        return {
+          ...state,
+          counter: state.counter - 1
+        }
+      case "INIT":
+        return {
+          ...state,
+          counter: action.payload
+        }  
+      default:
+        return state
+    }
+  }
+  
+  const initialState = {
+    counter: 0
+  }
+
+function Calculator() {
+    const [input, setInput] = useState(0)
+    const [state, dispatch] = useReducer(reducer, initialState)
+  
+    return (
+      <div>
+        <h1>Reducer Example</h1>
+  
+        <input type='number'
+               onChange={(e) => setInput(parseInt(e.target.value))}
+               value={input}
+        />
+        <br/>
+        <button onClick={() => dispatch({type: 'INIT', payload: input})}>Initialize Counter</button>
+  
+        <p>{ state.counter }</p>
+        <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
+        <button onClick={() => dispatch({type: 'DECREMENT'})}>Decrement</button>
+  
+      </div>
+    );
+}
+
+export default Calculator
